82b4055fa4733e45c71659ab1e2de26c1d9fd8f5
= <=>
2015-08-23 00:08:02 -0400

    Implemented both functions. I think this is the solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function returns a CacheMatrix which includes functions for setting and getting from a "cache"
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+		    message("Setting matrix")
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solution) inv <<- solution
+    getinverse <- function() inv
+    list(set = set, get = get,
+        setinverse = setinverse,
+        getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function uses CacheMatrices to read from a cache if the inverse has already been solved. Solves and writes the inverse to the cache on a cache miss.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("Getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
